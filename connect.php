8b851fc498edb619a305b3aa068ea923bddc8b6d
gicziunicorn <giczidani100@gmail.com>
2026-02-06 14:06:04 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+echo '<body style="background-color:black;color:white;">';
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "otletlada";
+$table = "otletek";
+
+try {
+    $conn = new mysqli($host, $user, $pass, $dbname);
+    if ($conn->connect_error) {
+        $_SESSION["error"] = "Connection failed: " . $conn->connect_error;
+        die("Connection failed: " . $conn->connect_error);
+        header("Location: failed.php");
+        exit;
+    }
+} catch (\Throwable $err) {
+    $_SESSION['error'] = (string) $err;
+    //echo "Hiba: " . $_SESSION['error'];
+    //echo '<p style="color:red;">Catched error: ' . $err . '</p>';
+    header("Location: failed.php");
+    exit;
+}
--- send.php
+++ send.php
@@ -1,3 +1,21 @@
 <?php
-echo $_POST["otlet"];
+require "connect.php";
+
+$otlet = $_POST["otlet"];
+$datum = date('Y-m-d H:i:s');
+$fnev = "cigany";
+
+$cmd = $conn->prepare("INSERT INTO otletek (id, otlet, datum, fnev, elfogadott, szavazatok) VALUES (NULL, ?, ?, ?, ?, ?)");
+$cmd->bind_param("sssbi", $otlet, $datum, $fnev, 0, 0);
+$cmd->execute();
+$result = $cmd->get_result();
+
+if ($result) {
+    $_SESSION["msg"] = "regsuccess"; //-successful registration
+    header("Location: loginpage.php");
+    exit;
+} else {
+    echo "Hiba történt!";
+}
+
 ?>
